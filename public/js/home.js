93e9ff4b4ad39ba2023226bd37335bdbb5956c0c
Alex Bennett <alexandergraybennett@gmail.com>
2016-02-28 13:40:14 -0700

    *

--- public/js/home.js
+++ public/js/home.js
@@ -14,21 +14,35 @@ $(document).ready(function(){
       // the magic
       responsive: [{
 
-          breakpoint: 1024,
+          breakpoint: 2000,
           settings: {
             slidesToShow: 3,
             infinite: true
           }
 
-        }, {
+        },
+
+        {
+
+            breakpoint: 1500,
+            settings: {
+              slidesToShow: 2,
+              infinite: true
+            }
+
+          },
+
+        {
 
           breakpoint: 600,
           settings: {
-            slidesToShow: 2,
-            dots: true
+            slidesToShow: 1,
+            dots: false
           }
 
-        }, {
+        },
+
+         {
           breakpoint: 300,
           settings: "unslick" // destroys slick
         }]
