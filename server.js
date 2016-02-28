b6b3b00fb907897cffe471d368a72965bf40ab88
Alex Bennett <alexandergraybennett@gmail.com>
2016-02-28 11:45:39 -0700

    adding nav

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-
-// set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
@@ -13,14 +11,10 @@ app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function(req, res) {
-
-	// ejs render automatically looks in the views folder
 	res.render('index');
 });
 
 app.get('/portfolio', function(req, res) {
-
-	// ejs render automatically looks in the views folder
 	res.render('portfolio');
 });
 
