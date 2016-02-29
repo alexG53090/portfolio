2a278c95d931bdb672cd3545366d4ee26c4045c3
Alex Bennett <alexandergraybennett@gmail.com>
2016-02-28 19:17:18 -0700

    *

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+    consumer_key: 'ZaPtUltCzS9nUFuSwrQYP0iQM',
+    consumer_secret: 'bkeFdOq18cpYCrtigKOrajuURqHsVYRKr6y6S2ve22rronNypt'
+};
+
+module.exports = config;
--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,9 @@
+var config = {
+    consumer_key: 'ZaPtUltCzS9nUFuSwrQYP0iQM',
+    consumer_secret: 'bkeFdOq18cpYCrtigKOrajuURqHsVYRKr6y6S2ve22rronNypt'
+};
+
+// module.exports = config;
 
 $(document).ready(function(){
   $('.triangle').hide();
@@ -56,11 +62,6 @@ function replay(){
     });
   });
 }
-
-
-$(body).scroll(function(){
-
-})
 
 function playSlides(){
   $('.my-slick-slider').slick({
