45f5218ae90bca618f3f8aefd93728691379f42e
Alex Bennett <alexandergraybennett@gmail.com>
2016-03-06 16:08:53 -0700

    adding random quotes

--- public/js/template.js
+++ public/js/template.js
@@ -2,10 +2,6 @@ $(document).ready(function(){
   animateLogo();
 })
 
-
-
-
-
 function animateLogo(){
   $('.square').hide();
   $('.circle').hide();
@@ -19,33 +15,32 @@ function animateLogo(){
   })
 }
 
-
 (function () {
-    $(document).ready(function () {
-        var quote;
-        quote = function () {
-            var API_KEY, url;
-            url = 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous';
-            API_KEY = 'NvBtBmdtSImshF8Ul6PZVcHM8rPtp1HIevIjsnu6b1nua1d11M';
-            $.ajax({
-                url: url,
-                type: 'PUT',
-                dataType: 'json',
-                beforeSend: function (xhr) {
-                    xhr.setRequestHeader('X-Mashape-Authorization', API_KEY);
-                },
-                success: function (data) {
-                    var twitterLink;
-                    document.getElementById('quote').innerHTML = data.quote + '<em id="author"> ' + data.author + '</em>';
-                    twitterLink = document.getElementById('tweet');
-                    twitterLink.href = 'https://twitter.com/intent/tweet?text=' + '"' + data.quote + '"' + ' --' + data.author;
-                    twitterLink.target = '_blank';
-                }
-            });
-        };
+  $(document).ready(function () {
+    var quote;
+    quote = function () {
+      var API_KEY, url;
+      url = 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous';
+      API_KEY = 'NvBtBmdtSImshF8Ul6PZVcHM8rPtp1HIevIjsnu6b1nua1d11M';
+      $.ajax({
+          url: url,
+          type: 'PUT',
+          dataType: 'json',
+          beforeSend: function (xhr) {
+              xhr.setRequestHeader('X-Mashape-Authorization', API_KEY);
+          },
+          success: function (data) {
+            var twitterLink;
+            document.getElementById('quote').innerHTML = data.quote + '<em id="author"> ' + "<br>" + data.author + '</em>';
+            twitterLink = document.getElementById('tweet');
+            twitterLink.href = 'https://twitter.com/intent/tweet?text=' + 'Look at my friend\'s personal website!' + ' http://alexbennett.tech/';
+            twitterLink.target = '_blank';
+          }
+        });
+      };
+      quote();
+      document.getElementById('btn').onclick = function () {
         quote();
-        document.getElementById('btn').onclick = function () {
-            quote();
-        };
-    });
+      };
+  });
 }.call(this));
