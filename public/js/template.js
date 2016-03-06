890d41349383e4286d35003fe55642ae92242e4d
Alex Bennett <alexandergraybennett@gmail.com>
2016-03-06 14:18:30 -0700

    trying to integrate quotes

--- public/js/template.js
+++ public/js/template.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
   animateLogo();
+  grabQuote();
 })
+
+function grabQuote(){
+  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+    $("body").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
+  });
+}
+
 
 function animateLogo(){
   $('.square').hide();
@@ -9,7 +17,7 @@ function animateLogo(){
   $('.square').fadeIn(200, function(){
     $('.circle').fadeIn(200, function(){
       $('.triangle').fadeIn(200, function(){
-        console.log('fuck you')
+        console.log('Hey you')
       })
     })
   })
