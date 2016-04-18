b6c42a0b03c1f58c1f1eabb557d4c780c4f41952
Alex Bennett <alexandergraybennett@gmail.com>
2016-04-17 19:49:47 -0600

    adjusting tween

--- public/js/app.js
+++ public/js/app.js
@@ -3,35 +3,50 @@ var controller = new ScrollMagic({
 });
 
 var parallax = new TimelineMax().add([
-  TweenMax.to(".logo", 5, {opacity: '0'}),
-  TweenMax.to(".greeting", 5, {opacity: '0'}),
-  TweenMax.to(".touch", 5, {opacity: '0'}),
-  TweenMax.to(".fa-angle-double-down", 5, {opacity: '0'}),
+  TweenMax.to(".logo", 5, {opacity: '0', y: '-75px'}),
+  TweenMax.to(".greeting", 5, {opacity: '0', y: '-75px'}),
+  TweenMax.to(".touch", 5, {opacity: '0', y: '-75px'}),
+  TweenMax.to(".fa-angle-double-down", 5, {opacity: '0', y: '-75px'}),
   TweenMax.to(".first-break", 5, {opacity: '0'}),
-  TweenMax.to(".projects", 10, {ease:Power1.easeOut}),
-  TweenMax.to(".projectos", 10, {ease:Power1.easeOut}),
-  TweenMax.to(".dash-wrap", 10, {ease:Power1.easeOut})
+  TweenMax.to(".projects", 20, {ease:Power1.easeOut}),
+  TweenMax.to(".projectos", 20, {ease:Power1.easeOut}),
+  TweenMax.to(".dash-wrap", 20, {ease:Power1.easeOut})
 ]);
 
 
 var parallaxScene = new ScrollScene({
   triggerElement: '.projects',
-  duration: 200
+  duration: 700
 })
 .setTween(parallax)
 .addTo(controller);
 
 var flyRight = new TimelineMax().add([
-  TweenMax.to(".dash-wrap", 1, {ease:Power1.easeOut, width: '+=400px'}),
-  TweenMax.to(".das-work", 1, {ease:Power1.easeOut, y: '-100px'}),
-  TweenMax.to(".dash-wrap", 1, {ease:Power1.easeOut, width: '+=400px'})
+  TweenMax.from(".das-work", 1, {ease:Power1.easeOut, y: '-75px'}),
+  TweenMax.to('.dash-wrap', 1, {width: "800px"})
 ])
 
 var project = new ScrollScene({
   triggerElement: '.projectos',
-  duration: 400
+  duration: 300
 })
 .setTween(flyRight)
+.addTo(controller);
+
+var me = new TimelineMax().add([
+  TweenMax.to('.about-me', 1, {y: '-100px'}),
+  TweenMax.to('.my-face', 1, {y: '-80px'}),
+  TweenMax.to('.me', 1, {y: '-80px'}),
+
+  // TweenMax.from('.my-face', 1, {y: '-80px'}),
+  // TweenMax.from('.me', 1, {y: '-80px'}),
+])
+
+var about = new ScrollScene({
+  triggerElement: '.my-face',
+  duration: 700
+})
+.setTween(me)
 .addTo(controller);
 
 $(document).ready(function() {
